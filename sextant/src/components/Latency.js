22b5976ca4047e400427af67092310b2c325571c
Bikrant226 <bikrantthakur6@gmail.com>
2022-07-30 21:40:37 +0530

    solved typo error

--- sextant/src/components/Latency.js
+++ sextant/src/components/Latency.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
 function Latency() {
-    const time=new Date()
     const [latency,setLatency]=useState('')
     const websoc=new WebSocket('ws://localhost:55455')
     websoc.onopen=()=>{
         console.log('connected')
     }
     websoc.onmessage = (message) => {
-        setLatency(time.getTime()-message.timeStamp);
+        setLatency(new Date().getTime()-message.data);
       };
   return (
     <div>

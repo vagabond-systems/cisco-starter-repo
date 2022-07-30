b837e6300a1d3720a6a66236bcbe021e5c741ca0
Bikrant226 <bikrantthakur6@gmail.com>
2022-07-30 20:11:21 +0530

    Create banner and exhibit component

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>Sextant</div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/components/Exhibit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Exhibit() {
+  return (
+    <div>Exhibit</div>
+  )
+}
+
+export default Exhibit

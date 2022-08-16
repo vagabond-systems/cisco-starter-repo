94c6e87696a123ee784bb7a0883b0501ed1c18aa
CPresha1 <79553588+CPresha1@users.noreply.github.com>
2022-08-16 07:19:04 -0400

    Testing...

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(Banner) {
+    return (
+        <div style ={{display: 'flex', justifyContent:'center', alignItems:'center', height:'100%'} }>
+            <h1>Hello, and Welcome to Sextant!{Banner.test}</h1>
+            </div>
+    );
 }
+
+class Exhibit extends React.Component {
+    render() {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50%' }}>
+                <h1>This is for our exhibit component!</h1>
+            </div>
+        );
+    }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Exhibit />);
 
 export default App;

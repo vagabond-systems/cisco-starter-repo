6b6a8f28a3e1c5a69ddd6b36e36d603edba3ada2
Alex Savi <alex.savi.kv@gmail.com>
2022-08-28 22:09:01 +0530

    Banner added

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,30 @@
 
 import './App.css';
+import Banner from "react-banner";
+import "./index.css";
+import "react-banner/dist/style.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Banner
+        logo="Sextant App"
+        url={window.location.pathname}
+        items={[
+          { content: "Home", url: "/example" },
+          { content: "Another", url: "/another" },
+          {
+            content: "Link w/ Children",
+            url: "/children",
+            children: [
+              { content: "John", url: "/children/john" },
+              { content: "Jill", url: "/children/jill" },
+              { content: "Jack", url: "/children/jack" }
+            ]
+          }
+        ]}
+      />
+    
     </div>
   );
 }

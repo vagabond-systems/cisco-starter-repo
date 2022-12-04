f3967f5d9429940f4f69ded8f1fbee792e5a91be
Sachin Shakya <108862093+mikesh-shakya@users.noreply.github.com>
2022-12-04 15:00:46 +0530

    task 2

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Banner = () => {
+  return <div>site’s title</div>;
+}
+
+export default Banner
---
+++ sextant/src/components/Exhibit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exhibit = () => {
+  return (
+    <>
+      <h1>Exhibit</h1>
+      <p>any</p>
+    </>
+  );
+}
+
+export default Exhibit

eee8a5a2ad6aa79d02bc34262cc76d7dbed1053a
Sviatoslav Pechenevskyi <svyatoslavpech@gmail.com>
2023-01-11 11:28:51 -0700

    I am created an exhibit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Title from './Title';
-
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Exhibit />
     </div>
   );
 }
---
+++ sextant/src/Exhibit.js
@@ -0,0 +1,15 @@
+// src/Exhibit.js
+
+import React from 'react'
+import './Exhibit.css';
+
+
+function Exhibit() {
+  return (
+    <div className="exhibit">
+        <h1>Exhibit</h1>
+    </div>
+  )
+}
+
+export default Exhibit

e42326b2f3b6e7ac9d1dd4e8bfeee3b1f0c69053
Divyesh Saroj <divyeshsaroj.com>
2023-03-07 23:50:46 +0530

    Updated

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import AddressDisplay from './AddressDisplay';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Banner bannerText="Sextant" />
-                <Exhibit name="I'm an exhibit!"></Exhibit>
-                <Exhibit name="I'm also an exhibit!"></Exhibit>
-                <Exhibit name="Don't forget about me, I'm an exhibit too!"></Exhibit>
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
             </div>
         );
     }

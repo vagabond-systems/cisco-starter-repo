fe127ce4c666f6b06f68ea04c17b1a1a539a8fb0
Divyesh Saroj <divyeshsaroj.com>
2023-03-07 23:40:31 +0530

    Updated

--- pylon/sextant/src/App.js
+++ pylon/sextant/src/App.js
@@ -1,31 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Banner from './Banner';
-import Exhibit from './Exhibit';
-import Map from './Map';
-import Quote from './Quote';
-import Image from './Image';
-import IPAddress from './IPAddress';
+
 function App() {
   return (
     <div className="App">
-      <Banner />
-      <Exhibit>
-      <Map />
-      </Exhibit>
-      <Exhibit>
-      <Quote />
-      </Exhibit>
-      <Exhibit>
-      <Image />
-      </Exhibit>
-      <Exhibit>
-        <IPAddress type="v4" />
-      </Exhibit>
-      <Exhibit>
-        <IPAddress type="v6" />
-      </Exhibit>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

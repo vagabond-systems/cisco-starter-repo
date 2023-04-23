ef5bb3ba696a8cc3547e95b35c35979dbf8be322
Sviatoslav Pechenevskyi <svyatoslavpech@gmail.com>
2023-04-23 01:16:04 -0600

    ip client implemented with axios

--- sextant/src/Exhibit.js
+++ sextant/src/Exhibit.js
@@ -2,12 +2,13 @@
 
 import React from 'react'
 import './Exhibit.css';
-
+import ClientIpv4 from './ClientIpv4';
 
 function Exhibit() {
   return (
     <div className="exhibit">
         <h1>Exhibit</h1>
+        <ClientIpv4 />
     </div>
   )
 }

86114be8a6db5c8d6e4a3bbd0f63ac11a804a890
madhu paka <madhupaka11@gmail.com>
2023-07-03 12:17:08 +0530

    third task

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import Banner from "./Banner";
 import Exhibit from "./Exhibit";
+import IpAdress from "./IpAdress";
 
 function App() {
   return (
     <div className="App">
       <Banner bannerName="Sextant" />
-      <Exhibit exhibitTitle="Exhibit">
-        <div>This is the card content</div>
-      </Exhibit>
-      <Exhibit exhibitTitle="IP Addresses">
-        <div>This space is for the IP addresses of the following devices</div>
+      <Exhibit exhibitTitle="IP Adresses">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Exhibit exhibitTitle="IPv4">
+            <IpAdress ipType="ipv6"></IpAdress>
+          </Exhibit>
+          <Exhibit exhibitTitle="IPv6">
+            <IpAdress ipType="ipv6"></IpAdress>
+          </Exhibit>
+        </div>
       </Exhibit>
     </div>
   );
---
+++ sextant/src/IpAdress.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+const IpAdress = ({ ipType }) => {
+    const [ipAddress, setIpAdress] = useState("");
+    const ipArray = { ipv4: "https://api.ipify.org?format=json", ipv6: "https://api64.ipify.org?format=json" }
+    useEffect(() => {
+        fetch(
+            "https://api64.ipify.org?format=json")
+            .then((res) => res.json())
+            .then((json) => {
+                setIpAdress(json.ip);
+                console.log(json);
+            })
+    }, []);
+
+    return (
+        <div>Your public IP Address: {ipAddress}</div>
+    )
+}
+
+export default IpAdress

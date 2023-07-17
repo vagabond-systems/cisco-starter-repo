d23a462adeee4d0112cc1d8ffb4e65648adcc949
krlaslm <khaiirulaslam@gmail.com>
2023-07-17 15:46:34 +0800

    Update App.js

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

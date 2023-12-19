9122839e00fdebb85d017aff67ef324e15924668
guledrafle7 <guledrafle7@gmail.com>
2023-12-19 17:48:42 +0000

    "Edited"

--- sextant/src/Exhibit.js
+++ sextant/src/Exhibit.js
@@ -6,7 +6,9 @@ class Exhibit extends Component{
     render(){
         return (
             <div className="Exhibit">
-                <h2 className="ExhibitHeading">{this.props.name}</h2>
+                <h2 className="ExhibitHeading">
+                    {this.props.name}
+                    </h2>
                 <div className="ExhibitContent">
                     {this.props.children}
                 </div>
